a84c937687e346c127939f7a6ed35a193ae1b253
aromalma <aromalma>
2023-09-12 12:56:39 +0530

    augmentation config added

--- config.py
+++ config.py
@@ -1,42 +1,66 @@
 def get_names(path):
-	with open(path,'r') as f:
-		t=f.read().split("\n")
-	names=[x for x in filter(lambda i : bool(len(i)),t)]
-	print(names)
-	return names
-config={
+    with open(path, 'r') as f:
+        t = f.read().split("\n")
+    names = [x for x in filter(lambda i: bool(len(i)), t)]
+    print(names)
+    return names
 
-	'model':'yolo_nas_m', # model
-	'CHECKPOINT_DIR' : 'checkpoints',
-	'experiment_name':'test',  # name of experiment
 
-	'dataset_params' : {
-	    'data_dir':'', # root of train and test folder
-	    'train_images_dir':'train/',
-	    'train_labels_dir':'train/',
-	    'test_images_dir':'test/',
-	    'test_labels_dir':'test/',
-	    'classes': get_names('/home/pavan/ksa/classes.txt') # path to classes.txt or obj.names
-	},
-	'dataloader_params':{
-	    'batch_size':4, # adjust based on your GPU
-	    'num_workers':4
-	},
-	'train_parms':{
-		'silent_mode': True,
-	    "average_best_models":True,
-	    "warmup_mode": "linear_epoch_step",
-	    "warmup_initial_lr": 1e-6,
-	    "lr_warmup_epochs": 3,
-	    "initial_lr": 5e-4,
-	    "lr_mode": "cosine",
-	    "cosine_final_lr_ratio": 0.1,
-	    "optimizer": "Adam",
-	    "optimizer_params": {"weight_decay": 0.0001},
-	    "zero_weight_decay_on_bias_and_bn": True,
-	    "ema": True,
-	    "ema_params": {"decay": 0.9, "decay_type": "threshold"},
-	    "max_epochs": 40,
-	    "mixed_precision": True,
-	}
+def transform_config(dim):
+    t = [
+    {'DetectionMosaic':{'input_dim': dim, "prob":0.5 ,"enable_mosaic":False,'border_value':114 }},
+                       {'DetectionRandomAffine': {'degrees': 6, 'translate': 0.05, 'scales': [0.7, 1.2], 'shear': 0.5,
+                                                  'target_size': dim, 'filter_box_candidates': True, 'wh_thr': 2,
+                                                  'area_thr': 0.1, 'ar_thr': 20}},
+    #                    {'DetectionRandomAffine': {'degrees': 0.0, 'translate': 0.1, 'scales': [.7,1.3], 'shear': 0,
+    #                                               'target_size': dim }},
+                       # {'DetectionMixup': {'input_dim': dim, 'mixup_scale': [0.7, 1.3], 'prob': 0.2, 'flip_prob': 0.2}},
+                       {'DetectionHSV': {'prob': 0.3, 'hgain': 5, 'sgain': 6, 'vgain': 6}},
+                       # {'DetectionHorizontalFlip': {'prob': 0}},
+                       {'DetectionPaddedRescale': {'input_dim': dim, 'pad_value': 114}},
+                       {'DetectionTargetsFormatTransform': {'input_dim': dim, 'output_format': 'LABEL_CXCYWH'}}
+        ]
+
+
+    return t
+
+
+config = {
+
+    'model': 'yolo_nas_m',  # model
+    'CHECKPOINT_DIR': 'checkpoints',
+    'experiment_name': 'test',  # name of experiment
+
+    'dataset_params': {
+        'data_dir': '/home/groot/Aromal/yolonas-train-infer/nn/yolonas_train/',  # root of train and test folder
+        'train_images_dir': 'train/',
+        'train_labels_dir': 'train/',
+        'test_images_dir': 'test/',
+        'test_labels_dir': 'test/',
+        'classes': get_names('/home/groot/Aromal/classes.txt'),  # path to classes.txt or obj.names
+        'input_dim': [352, 640],
+        'transforms': transform_config([352, 640])
+
+    },
+    'dataloader_params': {
+        'batch_size': 8,  # adjust based on your GPU
+        'num_workers': 4,
+    },
+    'train_parms': {
+        'silent_mode': False,
+        "average_best_models": True,
+        "warmup_mode": "linear_epoch_step",
+        "warmup_initial_lr": 1e-6,
+        "lr_warmup_epochs": 3,
+        "initial_lr": 5e-4,
+        "lr_mode": "cosine",
+        "cosine_final_lr_ratio": 0.1,
+        "optimizer": "Adam",
+        "optimizer_params": {"weight_decay": 0.0001},
+        "zero_weight_decay_on_bias_and_bn": True,
+        "ema": True,
+        "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+        "max_epochs": 10,
+        "mixed_precision": True,
+    }
 }
--- nas_trainer.py
+++ nas_trainer.py
@@ -4,7 +4,8 @@ from super_gradients.training import Trainer
 from super_gradients.training.metrics import Accuracy
 from super_gradients.training import dataloaders
 from super_gradients.training import models
-from super_gradients.training.dataloaders.dataloaders import coco_detection_yolo_format_train, coco_detection_yolo_format_val
+from super_gradients.training.dataloaders.dataloaders import coco_detection_yolo_format_train, \
+    coco_detection_yolo_format_val
 from super_gradients.training.losses import PPYoloELoss
 from super_gradients.training.metrics import DetectionMetrics_050
 from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
@@ -13,14 +14,14 @@ from opencv_draw_annotation import draw_bounding_box
 import numpy as np
 from config import config
 
-
-
 train_data = coco_detection_yolo_format_train(
     dataset_params={
         'data_dir': config['dataset_params']['data_dir'],
         'images_dir': config['dataset_params']['train_images_dir'],
         'labels_dir': config['dataset_params']['train_labels_dir'],
-        'classes': config['dataset_params']['classes']
+        'classes': config['dataset_params']['classes'],
+        'input_dim': config['dataset_params']['input_dim'],
+        "transforms": config['dataset_params']["transforms"]
     },
     dataloader_params=config['dataloader_params']
 )
@@ -30,63 +31,67 @@ test_data = coco_detection_yolo_format_val(
         'data_dir': config['dataset_params']['data_dir'],
         'images_dir': config['dataset_params']['test_images_dir'],
         'labels_dir': config['dataset_params']['test_labels_dir'],
-        'classes': config['dataset_params']['classes']
+        'classes': config['dataset_params']['classes'],
+        'input_dim': config['dataset_params']['input_dim']
     },
     dataloader_params=config['dataloader_params']
 )
 
 print(".............TRAIN DATA TRANSFORMS..............")
+# print(train_data.dataset)
 print(train_data.dataset.transforms)
+
 print()
 train_data.dataset.plot()
 test_data.dataset.plot()
-model = models.get(config['model'], 
-                   num_classes=len(config['dataset_params']['classes']), 
+model = models.get(config['model'],
+                   num_classes=len(config['dataset_params']['classes']),
                    pretrained_weights="coco",
                    )
 
 trainer = Trainer(experiment_name=config['experiment_name'], ckpt_root_dir=config['CHECKPOINT_DIR'])
 
-config['train_parms']['loss']=PPYoloELoss(
-        use_static_assigner=False,
+config['train_parms']['loss'] = PPYoloELoss(
+    use_static_assigner=False,
+    # NOTE: num_classes needs to be defined here
+    num_classes=len(config['dataset_params']['classes']),
+    reg_max=16
+)
+
+config['train_parms']["valid_metrics_list"] = [
+    DetectionMetrics_050(
+        score_thres=0.1,
+        top_k_predictions=300,
         # NOTE: num_classes needs to be defined here
-        num_classes=len(config['dataset_params']['classes']),
-        reg_max=16
-    )
-
-config['train_parms']["valid_metrics_list"]=[
-        DetectionMetrics_050(
-            score_thres=0.1,
-            top_k_predictions=300,
-            # NOTE: num_classes needs to be defined here
-            num_cls=len(config['dataset_params']['classes']),
-            normalize_targets=True,
-            post_prediction_callback=PPYoloEPostPredictionCallback(
-                score_threshold=0.01,
-                nms_top_k=1000,
-                max_predictions=300,
-                nms_threshold=0.7
-            )
+        num_cls=len(config['dataset_params']['classes']),
+        normalize_targets=True,
+        post_prediction_callback=PPYoloEPostPredictionCallback(
+            score_threshold=0.01,
+            nms_top_k=1000,
+            max_predictions=300,
+            nms_threshold=0.7
         )
-    ]
+    )
+]
 
-config['train_parms']["metric_to_watch"]='mAP@0.50'
+config['train_parms']["metric_to_watch"] = 'mAP@0.50'
 
-trainer.train(model=model, 
-              training_params=config['train_parms'], 
-              train_loader=train_data, 
+trainer.train(model=model,
+              training_params=config['train_parms'],
+              train_loader=train_data,
               valid_loader=test_data)
 
 best_model = models.get('yolo_nas_m',
                         num_classes=len(config['dataset_params']['classes']),
                         checkpoint_path=f"{config['CHECKPOINT_DIR']}/{config['experiment_name']}/ckpt_best.pth").cuda()
 trainer.test(model=best_model,
-            test_loader=test_data,
-            test_metrics_list=DetectionMetrics_050(score_thres=0.1, 
-                                                   top_k_predictions=300, 
-                                                   num_cls=len(config['dataset_params']['classes']), 
-                                                   normalize_targets=True, 
-                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01, 
-                                                                                                          nms_top_k=1000, 
-                                                                                                          max_predictions=300,                                                                              
-                                                                                                          nms_threshold=0.7)))
+             test_loader=test_data,
+             test_metrics_list=DetectionMetrics_050(score_thres=0.1,
+                                                    top_k_predictions=300,
+                                                    num_cls=len(config['dataset_params']['classes']),
+                                                    normalize_targets=True,
+                                                    post_prediction_callback=PPYoloEPostPredictionCallback(
+                                                        score_threshold=0.01,
+                                                        nms_top_k=1000,
+                                                        max_predictions=300,
+                                                        nms_threshold=0.7)))
